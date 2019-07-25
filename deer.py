5d5d7cb8a73467d475ea680947447a318fc3e94f
cainiaomama520 <1151682534@qq.com>
2019-07-25 11:15:13 +0800

    add regist api

--- deer.py
+++ deer.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
 import config
+from exts import db
+from models import User
 
 
 app = Flask(__name__)
 app.config.from_object(config)
+db.init_app(app)
 
 
 
@@ -26,7 +29,23 @@ def regist():
     if request.method == 'GET':
         return render_template('regist.html')
     else:
-        pass
+        telephone = request.form.get('telephone')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        user = User.query.filter(User.telephone == telephone).first()
+        if user:
+            return "该用户已经注册，请换一个手机号码！"
+        else:
+            if password1 != password2:
+                return "两次密码不一致，请重新输入密码！"
+            else:
+                user = User(username=username, telephone=telephone, password=password1)
+                db.session.add(user)
+                db.session.commit()
+                return redirect(url_for('login'))
+
 
 if __name__ == '__main__':
     app.run()
